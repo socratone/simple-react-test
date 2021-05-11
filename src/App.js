2682aee54ab79e886dde9d20617314c6cdbe0641
socratone <gim2origin@gmail.com>
2021-05-12 00:16:52 +0900

    react router 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Basic from './components/Basic';
 
 function App() {
-  return <div className="App"><Basic /></div>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Basic</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/">
+            <Basic />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
